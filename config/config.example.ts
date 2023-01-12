e22dc4fd49e301ac31abfe75afc256226ec82070
Wojtek <rossa21@gmail.com>
2023-01-12 01:34:35 +0100

    Fixed missing id in book-record. Plus code clean up.

--- config/config.example.ts
+++ config/config.example.ts
@@ -1,9 +1,9 @@
-const example = {
-    dbHost: 'yourHostName',
-    dbUser: 'userName',
-    dbDatabase: 'databaseName',
-    dbPassword: "",
-    corsOrigin: 'http://localhost:3000',
-    namedPlaceholders: true,
-    decimalNumbers: true,
-}
+// const config = {
+//     dbHost: 'yourHostName',
+//     dbUser: 'userName',
+//     dbDatabase: 'databaseName',
+//     dbPassword: "",
+//     corsOrigin: 'http://localhost:3000',
+//     namedPlaceholders: true,
+//     decimalNumbers: true,
+// }
